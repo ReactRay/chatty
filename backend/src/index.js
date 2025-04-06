4ef664551bc7ebd53d95022f5214fdac19906ea2
ReactRay <raydiaz1899@gmail.com>
2025-04-06 12:34:10 +0300

    add backend connection to mongo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,17 @@
 import express from 'express'
+import authRoutes from './routes/auth.route.js'
+import dotenv from 'dotenv'
+import { connectDB } from './lib/db.js'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5001
 
 const app = express()
 
+app.use('/api/auth', authRoutes)
+
 app.listen(5001, () => {
-  console.log(`server is running on port 5001`)
+  console.log(`server is running on port ${PORT}`)
+  connectDB()
 })
