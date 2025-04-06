310c6e7e34a7de30ddfe91a43250432b7c138923
ReactRay <raydiaz1899@gmail.com>
2025-04-06 12:50:06 +0300

    add hashed passwords and token generation

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,11 @@ import authRoutes from './routes/auth.route.js'
 import dotenv from 'dotenv'
 import { connectDB } from './lib/db.js'
 
+const app = express()
 dotenv.config()
+app.use(express.json())
 
 const PORT = process.env.PORT || 5001
-
-const app = express()
 
 app.use('/api/auth', authRoutes)
 
