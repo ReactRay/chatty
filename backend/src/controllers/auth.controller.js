da6b254ac53b0575a560c231df867c28571f49f4
ReactRay <raydiaz1899@gmail.com>
2025-04-06 13:19:02 +0300

    finish login/logout/signup functions in api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,13 @@
+import { generateToken } from '../lib/utils.js'
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
 
 export async function signup(req, res) {
   const { fullName, email, password } = req.body
+
+  if (!fullName || !email || !password) {
+    return res.status(400).json({ message: 'All fields are required' })
+  }
 
   try {
     if (password.length < 6) {
@@ -25,17 +30,62 @@ export async function signup(req, res) {
       password: hashedPassword,
     })
 
-    if (newUSer) {
+    if (newUser) {
+      generateToken(newUser._id, res)
+      await newUser.save()
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      })
     } else {
       res.status(400).json({ message: 'invalid user data' })
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log('Error in signup controller', error.message)
+    res.status(500).json({ message: 'Internal Server Error' })
+  }
 }
 
-export function login(req, res) {
-  res.send('login')
+export async function login(req, res) {
+  const { email, password } = req.body
+
+  try {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(400).json({ message: 'invalid credintials' })
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: 'invalid credintials' })
+    }
+
+    generateToken(user._id, res)
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    })
+  } catch (error) {
+    console.log('error in login controller', error.message)
+  }
 }
 
 export function logout(req, res) {
-  res.send('logout')
+  try {
+    res.cookie('jwt', '', { maxAge: 0 })
+    res.status(200).json({ message: 'logged out successfully' })
+  } catch (error) {
+    console.log('Error in checkAuth controller', error.message)
+    res.status(500).json({ message: 'Internal Server Error' })
+  }
 }
+
+export async function updateProfile(req, res) {}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,5 +7,6 @@ const router = express()
 router.post('/signup', signup)
 router.post('/login', login)
 router.post('/logout', logout)
+// router.put('/update-profile', updateProfile)
 
 export default router
