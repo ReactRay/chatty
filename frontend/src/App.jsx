0ed3846d25472483bdb631e9517a1a546fd40d89
ReactRay <raydiaz1899@gmail.com>
2025-04-06 15:48:13 +0300

    add pages to front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,28 @@
 
-
+import NavBar from "./components/NavBar"
+import { Routes, Route } from "react-router-dom"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
+import SettingsPage from "./pages/SettingsPage"
+import ProfilePage from "./pages/ProfilePage"
+import HomePage from "./pages/HomePage"
 function App() {
 
   return (
-    <>
-      <div className="text-red-500">
-        App
 
-        <button class="btn btn-primary">One</button>
-        <button class="btn btn-secondary">Two</button>
-        <button class="btn btn-accent btn-outline">Three</button>
 
-      </div>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/settings' element={<SettingsPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
 
-    </>
+    </div>
+
   )
 }
 
---
+++ frontend/src/pages/SettingsPage.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+const SettingsPage = () => {
+    return (
+        <div>
+            Settings
+        </div>
+    )
+}
+
+export default SettingsPage
